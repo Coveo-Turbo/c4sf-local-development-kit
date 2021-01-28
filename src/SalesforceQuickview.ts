368e3d146afa2a97c8e67877ab4c40ce0fcca997
Julian Wolfe <jwolfe@coveo.com>
2021-01-28 14:04:52 -0500

    Implement Quickview-specific bindings and support ootb quickview options

--- src/SalesforceQuickview.ts
+++ src/SalesforceQuickview.ts
@@ -1,14 +1,14 @@
-import { Component, ComponentOptions, IComponentBindings, load } from 'coveo-search-ui';
+import { Component, ComponentOptions, IQuickviewOptions, IResultsComponentBindings, load } from 'coveo-search-ui';
 import { lazyDependentComponent } from '@coveops/turbo-core';
 
-export interface ISalesforceQuickviewOptions {}
+export interface ISalesforceQuickviewOptions extends IQuickviewOptions {}
 
 @lazyDependentComponent('Quickview')
 export class SalesforceQuickview extends Component {
     static ID = 'SalesforceQuickview';
     static options: ISalesforceQuickviewOptions = {};
 
-    constructor(public element: HTMLElement, public options: ISalesforceQuickviewOptions, public bindings: IComponentBindings) {
+    constructor(public element: HTMLElement, public options: ISalesforceQuickviewOptions, public bindings: IResultsComponentBindings) {
         super(element, SalesforceQuickview.ID, bindings);
         this.options = ComponentOptions.initComponentOptions(element, SalesforceQuickview, options);
         load("Quickview").then(() => this.build(element, options, bindings))
